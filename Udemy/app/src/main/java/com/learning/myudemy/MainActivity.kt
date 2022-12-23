e264f309ffa8faed7b75ec33337c091559ed8f24
gram15inch <94951889+gram15inch@users.noreply.github.com>
2022-12-23 15:55:09 +0900

    ui - toolbar 연결

--- Udemy/app/src/main/java/com/learning/myudemy/MainActivity.kt
+++ Udemy/app/src/main/java/com/learning/myudemy/MainActivity.kt
@@ -3,9 +3,11 @@ package com.learning.myudemy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.findFragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.learning.myudemy.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,13 +17,17 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     lateinit var navHostFragment : NavHostFragment
     lateinit var navController: NavController
-
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        //navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        navHostFragment = binding.mainNavHostFragment.getFragment()
         navController = navHostFragment.findNavController()
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(binding.mainBottomNav.menu)
 
+        binding.mainAppBarConfiguration = appBarConfiguration
         binding.mainNavController = navController
     }
 }
--- Udemy/app/src/main/java/com/learning/myudemy/ui/recommendation/RecommendationFragment.kt
+++ Udemy/app/src/main/java/com/learning/myudemy/ui/recommendation/RecommendationFragment.kt
@@ -6,10 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.ui.AppBarConfiguration
+import com.learning.myudemy.MainActivity
 import com.learning.myudemy.R
+import com.learning.myudemy.databinding.FragmentRecommendationBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class RecommendationFragment : Fragment() {
 
+    lateinit var binding : FragmentRecommendationBinding
     companion object {
         fun newInstance() = RecommendationFragment()
     }
@@ -20,13 +30,15 @@ class RecommendationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_recommendation, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_recommendation, container, false)
+        binding.testName =" test name"
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(RecommendationViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 
 }
