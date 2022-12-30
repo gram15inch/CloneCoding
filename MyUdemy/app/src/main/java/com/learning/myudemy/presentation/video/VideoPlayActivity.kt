2634cc4d6b26edee6eddac48ac6bcfa1957f3c46
gram15inch <94951889+gram15inch@users.noreply.github.com>
2022-12-31 08:14:42 +0900

    ui - 비디오뷰 변경

--- MyUdemy/app/src/main/java/com/learning/myudemy/presentation/video/VideoPlayActivity.kt
+++ MyUdemy/app/src/main/java/com/learning/myudemy/presentation/video/VideoPlayActivity.kt
@@ -1,27 +1,85 @@
 package com.learning.myudemy.presentation.video
 
-import android.net.Uri
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.MediaController
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.learning.myudemy.R
 import com.learning.myudemy.databinding.ActivityVideoPlayBinding
 import com.learning.myudemy.presentation.base.LifecycleActivity
 
 class VideoPlayActivity : LifecycleActivity() {
-    lateinit var binding :ActivityVideoPlayBinding
+    lateinit var binding: ActivityVideoPlayBinding
+    private var player: SimpleExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition = 0L
+
+    private fun initializePlayer() {
+        player = SimpleExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                binding.videoPlayMain.player = exoPlayer
+
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+                exoPlayer.setMediaItem(mediaItem)
+
+                val secondMediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3));
+                exoPlayer.addMediaItem(secondMediaItem);
+
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentWindow, playbackPosition)
+                exoPlayer.prepare()
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_video_play)
+    }
 
-        val VIDEO_PATH = "android.resource://" + packageName + "/" + R.raw.vid_sample_sound
+    override fun onStart() {
+        super.onStart()
+        
+    }
+    public override fun onResume() {
+        super.onResume()
+        if (player == null) {
+            initializePlayer()
+        }
+    }
 
-        var uri: Uri = Uri.parse(VIDEO_PATH)
+    public override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
 
-        binding.videoPlayMain.setVideoURI(uri)
-        binding.videoPlayMain.setMediaController(MediaController(this))     // 없으면 에러
-        binding.videoPlayMain.requestFocus()    // 준비하는 과정을 미리함
+    public override fun onStop() {
+        super.onStop()
 
+    }
+
+    @SuppressLint("InlinedApi")
+    private fun hideSystemUi() {
+        binding.videoPlayMain.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+
+    private fun releasePlayer() {
+        player?.run {
+            playbackPosition = this.currentPosition
+            currentWindow = this.currentWindowIndex
+            playWhenReady = this.playWhenReady
+            release()
+        }
+        player = null
     }
 }
